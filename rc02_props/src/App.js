f39ee6d48fc998d9e34b768681b72abf42644734
adem kalfalı <ademkalfa9@gmail.com>
2022-10-09 17:25:57 +0300

    JSX_Study

--- rc02_props/src/App.js
+++ rc02_props/src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Msg from "./components/Msg";
+import Msg from "./Msg";
 
-function App() {
+const Person = (props) => {
+  const { name, img, tel } = props;
   return (
-    <div>
-      <Msg name="Felix" age={5} />
-      <Msg name="Harvey" />
-      <Msg name="Helen" />
+    <div style={{ textAlign: "center" }}>
+      <Msg name={name} />
+      <img style={{ width: "200px" }} src={img} alt="img1" />
+      <p>{tel}</p>
     </div>
   );
-}
+};
 
-export default App;
+export default Person;
---
+++ rc02_props/src/components/Person.jsx
@@ -0,0 +1,12 @@
+const Person = () => {
+  return;
+  <div>
+    <p>Merhaba Adam</p>
+    <img
+      src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg"
+      alt="img1"
+    />
+    <p>5555555555</p>
+  </div>;
+};
+export default Person;
