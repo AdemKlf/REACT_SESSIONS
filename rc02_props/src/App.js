cb137ce718728f7c76e78a35ded27e43cafcebc7
adem kalfalı <ademkalfa9@gmail.com>
2022-10-10 15:25:29 +0300

    Props_Study

--- rc02_props/src/App.js
+++ rc02_props/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 // import Msg from "./components/Msg";
 import Person from "./components/Person";
-// import data from "./data";
+import data from "./data";
 
 //? Otomatik import çift tık + ctrl space
 
 function App() {
   return (
     <div>
-      <Person />
+      <Person name={data[0].name} img={data[0].img} tel={data[0].tel} />
+      <Person name={data[1].name} img={data[1].img} tel={data[1].tel} />
+      <Person name={data[2].name} img={data[2].img} tel={data[2].tel} />
     </div>
     // <div>
     //   {data.map((person) => (
