daa73542d0b397093df87a72662a724b4987d947
adem kalfalı <ademkalfa9@gmail.com>
2022-10-12 20:51:49 +0300

    React_events

---
+++ rc05_events_useStateHook/src/components/1-events/Events.jsx
@@ -0,0 +1,49 @@
+// * =======================================================
+// *                     EVENTS
+// * =======================================================
+
+//? ReactJS, Tarayicilar arasi uyumluluk ve performans artisi gibi
+//? sebeplerden oturu Sentetik Event olarak adilandirilan Olaylari
+//? kullanir. Sentetik Event, aslinda tarayicinin dogal event'larinin
+//? bir sarmalayici (Wrapper) arabirimle ortulmesi ile olusur. Bu sayede,
+//? React ortaminda kullanilan event'larin bilindik tarayicilarda
+//? sorunsuz calismasini saglanir.
+//? Ayrinti icin : https://reactjs.org/docs/events.html
+
+const Events = () => {
+  let message = "EVENT BASED PROGRAM";
+  const handleClick = () => {
+    alert("Btn Clicked");
+  };
+  const handleClear = (msg) => {
+    alert(msg);
+  };
+  const handleChange = (e) => {
+    console.log(e.target);
+
+    message = "REACT";
+    console.log(message);
+  };
+  return (
+    <div className="container text-center mt-4">
+      <h1>{message}</h1>
+      <button onClick={handleClick} className="btn  btn-success">
+        Click
+      </button>
+      <button
+        onClick={() => handleClear("Clear Btn Clicked")}
+        className="btn btn-dark"
+      >
+        Clear
+      </button>
+      <button onClick={handleChange} className="btn btn-danger">
+        Change
+      </button>
+      {/* <button onClick={(e) => handleChange(e)} className="btn btn-danger">
+        Change
+      </button> */}
+    </div>
+  );
+};
+
+export default Events;
