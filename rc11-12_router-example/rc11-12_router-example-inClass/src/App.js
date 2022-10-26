a9f5cfc64e36d87b82158a3d7f5037e84ef78df5
adem kalfalı <ademkalfa9@gmail.com>
2022-10-26 22:54:14 +0300

    Router_study

--- rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
+++ rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
@@ -7,6 +7,10 @@ import NotFound from "./pages/NotFound";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Paths from "./pages/Paths";
 import PersonDetail from "./pages/PersonDetail";
+import FullStack from "./pages/FullStack";
+import Aws from "./pages/Aws";
+import PrivateRouter from "./pages/PrivateRouter";
+import Login from "./pages/Login";
 
 //? Link, NavLink ve Navigate componentleri declerative routing
 //? gerceklestirmek icin kullanilir.
@@ -30,9 +34,19 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/people" element={<People />} />
         <Route path="/people/:id" element={<PersonDetail />} />
-        <Route path="/paths" element={<Paths />} />
-        <Route path="/contact" element={<Contact />} />
+
+        <Route path="paths" element={<Paths />}>
+          <Route index element={<FullStack />} />
+          {/* <Route path="fullstack" element={<FullStack />} /> */}
+          <Route path="aws" element={<Aws />} />
+        </Route>
+
+        <Route path="contact" element={<PrivateRouter />}>
+          <Route path="" element={<Contact />} />
+        </Route>
+        <Route path="login" element={<Login />} />
         {/* <Route path="*" element={<NotFound />} /> */}
+
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
 
