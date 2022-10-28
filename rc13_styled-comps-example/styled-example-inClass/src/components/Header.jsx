e79d88caa199ec6ae30879a211e0999db655b55b
adem kalfalı <ademkalfa9@gmail.com>
2022-10-28 15:11:26 +0300

    Add

--- rc13_styled-comps-example/styled-example-inClass/src/components/Header.jsx
+++ rc13_styled-comps-example/styled-example-inClass/src/components/Header.jsx
@@ -16,17 +16,15 @@ const Header = () => {
 
       <Flex>
         <div>
-          <div>
-            <h1>The IT Career of Your Dreams Starts Here!</h1>
-            <p>
-              Clarusway is a leading international software Bootcamp. Join a
-              micro class online with other trainees and learn coding skills
-              with a highly-skilled instructor.
-            </p>
-            <Button bg="#A62440">Start Your New Carier</Button>
-          </div>
-          <Image src="./images/hero.jpg" />
+          <h1>The IT Career of Your Dreams Starts Here!</h1>
+          <p>
+            Clarusway is a leading international software Bootcamp. Join a micro
+            class online with other trainees and learn coding skills with a
+            highly-skilled instructor.
+          </p>
+          <Button bg="#A62440">Start Your New Carier</Button>
         </div>
+        <Image src="./images/hero.jpg" />
       </Flex>
     </StyledHeader>
   );
