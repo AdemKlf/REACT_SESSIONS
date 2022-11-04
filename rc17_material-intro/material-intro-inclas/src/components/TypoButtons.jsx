f4675667dad3bc5affa7641b9f829c033d6e8ad4
adem kalfalı <ademkalfa9@gmail.com>
2022-11-04 14:55:48 +0300

    Typography Add

--- rc17_material-intro/material-intro-inclas/src/components/TypoButtons.jsx
+++ rc17_material-intro/material-intro-inclas/src/components/TypoButtons.jsx
@@ -3,7 +3,7 @@ import Typography from "@mui/material/Typography";
 const TypoButtons = () => {
   return (
     <div>
-      <Typography variant="h3" component="h6">
+      <Typography variant="h4" component="h6" align="center">
         MUI Typography
       </Typography>
       ;
