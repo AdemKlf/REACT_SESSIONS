afc30e2bcf7f9fa7a24ecb32306889cd41c923f5
adem kalfalı <ademkalfa9@gmail.com>
2022-11-04 14:50:01 +0300

    Material-İntro

---
+++ rc17_material-intro/material-intro-inclas/src/components/TypoButtons.jsx
@@ -0,0 +1,14 @@
+import Typography from "@mui/material/Typography";
+
+const TypoButtons = () => {
+  return (
+    <div>
+      <Typography variant="h3" component="h6">
+        MUI Typography
+      </Typography>
+      ;
+    </div>
+  );
+};
+
+export default TypoButtons;
