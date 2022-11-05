e093caed41ce58fc9e791e91144f48b93e1b5af3
adem kalfalı <ademkalfa9@gmail.com>
2022-11-05 12:45:44 +0300

    TextFieldComp-Study

--- rc17_material-intro/material-intro-inclass/src/components/TextFieldComp.jsx
+++ rc17_material-intro/material-intro-inclass/src/components/TextFieldComp.jsx
@@ -5,16 +5,39 @@ import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
 import DeleteIcon from "@mui/icons-material/Delete";
 import SendIcon from "@mui/icons-material/Send";
+import { useState } from "react";
 
 const TextFieldComp = () => {
+  const [error, Seterror] = useState(false);
   return (
     <div>
       <Container>
         <Typography variant="h4" color="error" align="center" mt={4}>
           TEXT FIELD
         </Typography>
-        <Box>
-          <TextField />
+        <Box sx={{ mt: 4 }} textAlign="center">
+          <TextField
+            margin="dense"
+            id="email"
+            label="Email"
+            placeholder="Enter your email"
+            fullWidth
+            error={error}
+            helperText={error && "Incorrect email format"}
+          />
+
+          <TextField
+            margin="normal"
+            id="password"
+            label="Password"
+            placeholder="Enter your password"
+            fullWidth
+            error={error}
+            helperText={error && "Incorrect password"}
+          />
+          <Button variant="contained" color="warning" sx={{ mt: 3 }}>
+            Submit
+          </Button>
         </Box>
       </Container>
     </div>
