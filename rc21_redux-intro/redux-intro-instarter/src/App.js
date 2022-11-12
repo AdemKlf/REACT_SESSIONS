e220ccf19cffce19a4ded9b56d49f381cc13a533
adem kalfalı <ademkalfa9@gmail.com>
2022-11-12 13:04:50 +0300

    redux-project

--- rc21_redux-intro/redux-intro-instarter/src/App.js
+++ rc21_redux-intro/redux-intro-instarter/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Counter from "./components/counter/Counter";
 import Todo from "./components/todo/Todo";
+import { legacy_createStore } from "redux";
 
 function App() {
+  const store = creatStore();
   return (
     <div className="app">
       <Counter />
-      <Todo />
+      {/* <Todo /> */}
     </div>
   );
 }
